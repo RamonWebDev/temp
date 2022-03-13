0cc0b4280541a202027441fd2a0d7a9d460e96cb
RamonWebDev <ramonplaysgames@gmail.com>
2022-03-12 19:20:47 -0600

    added geolocation

--- app.js
+++ app.js
@@ -39,6 +39,64 @@ let weather = {
     }
 };
 
+let geocode = {
+    reverseGeocode: function (latitude, longitude) {
+      var apikey = "ca43f577504d4dab9a0470c7d1d8fee8";
+  
+      var api_url = "https://api.opencagedata.com/geocode/v1/json";
+  
+      var request_url =
+        api_url +
+        "?" +
+        "key=" +
+        apikey +
+        "&q=" +
+        encodeURIComponent(latitude + "," + longitude) +
+        "&pretty=1" +
+        "&no_annotations=1";
+  
+      var request = new XMLHttpRequest();
+      request.open("GET", request_url, true);
+  
+      request.onload = function () {
+
+  
+        if (request.status == 200) {
+          // Success!
+          var data = JSON.parse(request.responseText);
+          weather.fetchWeather(data.results[0].components.city);
+          console.log(data.results[0].components.city)
+        } else if (request.status <= 500) {
+          // We reached our target server, but it returned an error
+  
+          console.log("unable to geocode! Response code: " + request.status);
+          var data = JSON.parse(request.responseText);
+          console.log("error msg: " + data.status.message);
+        } else {
+          console.log("server error");
+        }
+      };
+  
+      request.onerror = function () {
+        // There was a connection error of some sort
+        console.log("unable to connect to server");
+      };
+  
+      request.send(); // make the request
+    },
+    getLocation: function() {
+      function success (data) {
+        geocode.reverseGeocode(data.coords.latitude, data.coords.longitude);
+      }
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(success, console.error);
+      }
+      else {
+        weather.fetchWeather("Huston");
+      }
+    }
+  };
+
 document.querySelector(".search button").addEventListener("click", function(){
     weather.search();
 });
@@ -49,4 +107,4 @@ document.querySelector(".search-bar").addEventListener("keyup", function(event){
     } 
  });
 
- weather.fetchWeather("Huston");
+ geocode.getLocation();
